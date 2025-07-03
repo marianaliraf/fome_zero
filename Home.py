704963e6b1f67f31acc3958d0e687604e5614429
Mariana Lira <85141413+marianaliraf@users.noreply.github.com>
2025-07-03 07:57:46 -0300

    Add files via upload

--- Home.py
+++ Home.py
@@ -4,7 +4,6 @@ import streamlit as st
 import numpy as np 
 import pandas as pd
 from PIL import Image
-from streamlit_timeline import timeline
 import plotly.express as px
 import plotly.figure_factory as ff
 import requests
